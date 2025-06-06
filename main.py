b37e8c58bf8fd60ca58e0325a94cec30dc7a7964
rajaabdullahnasir <rajasofiyan2017@gmail.com>
2025-06-06 23:54:29 +0500

    my first changing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dns_enum import get_dns_records
 domain = "google.com"
 results = get_dns_records(domain)
 
-print ("===== DNS ENUMERATION RESULTS =====\n")
+print ("====== DNS ENUMERATION RESULTS ======\n")
 
 for record_type, values in results.items():
     print(f"record_type Records:")
