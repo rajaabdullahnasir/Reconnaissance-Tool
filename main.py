97c7a415c8fdcfe1930752c359391a41094c6c39
Abdullah Nasir <rajasofiyan2017@gmail.com>
2025-06-06 23:41:14 +0500

    dns_enum

---
+++ main.py
@@ -0,0 +1,11 @@
+from dns_enum import get_dns_records
+
+domain = "google.com"
+results = get_dns_records(domain)
+
+print ("===== DNS ENUMERATION RESULTS =====\n")
+
+for record_type, values in results.items():
+    print(f"record_type Records:")
+    for value in values:
+        print (f"-{value}")
